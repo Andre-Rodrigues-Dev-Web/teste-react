2d84d440e276ef5e7b6f6e82e0f4de3e2b9b5be3
Gabriel Valeriano <53843213+Lummidev@users.noreply.github.com>
2025-09-04 10:43:52 -0300

    feat(api): Add CORS

--- src-api/src/index.ts
+++ src-api/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { newsController } from "./Controller/NewsController";
-
+import cors from "cors";
 const port = 8000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.get("/", (_, res) => {
   res.status(200).json({ msg: "API" });
