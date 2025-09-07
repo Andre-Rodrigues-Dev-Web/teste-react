e7c6178b761d3060b43e3763bc613ff4f12d245f
Gabriel Valeriano <53843213+Lummidev@users.noreply.github.com>
2025-09-07 09:49:39 -0300

    feat(portfolio): Improve header responsiveness

--- src-portfolio/src/Components/AccountLoginIndicator/AccountLoginIndicator.tsx
+++ src-portfolio/src/Components/AccountLoginIndicator/AccountLoginIndicator.tsx
@@ -1,16 +1,35 @@
 import { FaRightFromBracket, FaRightToBracket, FaUser } from "react-icons/fa6";
 import "./AccountLoginIndicator.css";
 import { Link } from "react-router";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Context/AuthContext";
 import { truncateText } from "../../util";
 export const AccountLoginIndicator = () => {
   const auth = useContext(AuthContext);
   const [hovering, setHovering] = useState(false);
+  const [hideText, setHideText] = useState(
+    window.matchMedia("(min-width: 50rem)").matches,
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 50rem)")
+      .addEventListener("change", (e) => setHideText(e.matches));
+  }, []);
+
+  useEffect(() => {
+    console.log(hideText);
+  }, [hideText]);
   return (
     <>
       {auth.userInfo ? (
         <button
+          style={{
+            minWidth:
+              !auth.userInfo || !hideText
+                ? "7rem"
+                : `${truncateText(auth.userInfo.name, 20).length > 7 ? truncateText(auth.userInfo.name, 20).length : 7}rem`,
+          }}
           type="button"
           onClick={() => {
             auth.events
@@ -30,17 +49,22 @@ export const AccountLoginIndicator = () => {
         >
           {hovering ? (
             <>
-              <FaRightFromBracket /> <div>Sair?</div>
+              <FaRightFromBracket />
+              <span className="login-indicator-text">Sair?</span>
             </>
           ) : (
             <>
-              <FaUser /> {truncateText(auth.userInfo.name, 10)}
+              <FaUser />
+              <span className="login-indicator-text">
+                {truncateText(auth.userInfo.name, 20)}
+              </span>
             </>
           )}
         </button>
       ) : (
         <Link to="/login" className="account-login-indicator">
-          <FaRightToBracket /> Entrar
+          <FaRightToBracket />
+          <span className="login-indicator-text">Entrar</span>
         </Link>
       )}
     </>
