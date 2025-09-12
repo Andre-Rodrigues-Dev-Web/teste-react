21cb9e2a8780bb69dfea3796966da24103cfb7f1
Gabriel Valeriano <53843213+Lummidev@users.noreply.github.com>
2025-09-11 22:03:52 -0300

    feat(portfolio): Add account button dropdown menu

--- src-portfolio/src/Components/AccountLoginIndicator/AccountLoginIndicator.tsx
+++ src-portfolio/src/Components/AccountLoginIndicator/AccountLoginIndicator.tsx
@@ -6,61 +6,68 @@ import { AuthContext } from "../../Context/AuthContext";
 import { truncateText } from "../../util";
 export const AccountLoginIndicator = () => {
   const auth = useContext(AuthContext);
-  const [hovering, setHovering] = useState(false);
-  const [hideText, setHideText] = useState(
-    window.matchMedia("(min-width: 50rem)").matches,
+  const [showMenu, setShowMenu] = useState(false);
+  const [smallScreen, setSmallScreen] = useState(
+    window.matchMedia("(max-width: 30rem)").matches,
   );
-
+  const [smallerScreen, setSmallerScreen] = useState(
+    window.matchMedia("(max-width: 20rem)").matches,
+  );
   useEffect(() => {
     window
-      .matchMedia("(min-width: 50rem)")
-      .addEventListener("change", (e) => setHideText(e.matches));
+      .matchMedia("(max-width: 30rem)")
+      .addEventListener("change", (e) => setSmallScreen(e.matches));
+    window
+      .matchMedia("(max-width: 20rem)")
+      .addEventListener("change", (e) => setSmallerScreen(e.matches));
   }, []);
+
+  const truncatedName = auth.userInfo
+    ? truncateText(
+        auth.userInfo.name,
+        smallerScreen ? 4 : smallScreen ? 10 : 20,
+      )
+    : "";
 
   return (
     <>
       {auth.userInfo ? (
-        <button
-          style={{
-            minWidth:
-              !auth.userInfo || !hideText
-                ? "7rem"
-                : `${truncateText(auth.userInfo.name, 20).length > 7 ? truncateText(auth.userInfo.name, 20).length : 7}rem`,
-          }}
-          type="button"
-          onClick={() => {
-            auth.events
-              .onLogout()
-              .then(() => {
-                setHovering(false);
-              })
-              .catch((e) => console.log(e));
-          }}
-          onMouseOver={() => {
-            setHovering(true);
-          }}
-          onMouseOut={() => {
-            setHovering(false);
-          }}
-          className="account-login-indicator logged-in"
-          aria-label="Sair"
-        >
-          {hovering ? (
-            <>
-              <FaRightFromBracket />
-              <span className="login-indicator-text">Sair?</span>
-            </>
-          ) : (
-            <>
+        <>
+          <div className="account-login-indicator">
+            <button
+              onClick={() => {
+                setShowMenu(!showMenu);
+              }}
+              type="button"
+              className={`account-button ${showMenu ? "open" : ""}`}
+            >
               <FaUser />
-              <span className="login-indicator-text">
-                {truncateText(auth.userInfo.name, 20)}
-              </span>
-            </>
-          )}
-        </button>
+              {truncatedName}
+            </button>
+            {showMenu ? (
+              <div className="account-menu">
+                <button
+                  onClick={() => {
+                    auth.events
+                      .onLogout()
+                      .then(() => {
+                        setShowMenu(false);
+                      })
+                      .catch((e) => console.log(e));
+                  }}
+                  type="button"
+                  className="logout"
+                >
+                  <FaRightFromBracket /> Sair
+                </button>
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+        </>
       ) : (
-        <Link to="/login" className="account-login-indicator">
+        <Link to="/login" className="account-login-indicator login">
           <FaRightToBracket />
           <span className="login-indicator-text">Entrar</span>
         </Link>
